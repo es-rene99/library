73af40e9e637437edaec747e5288ca664f366102
es-rene99 <es.rene99@gmail.com>
2021-05-04 06:35:52 -0600

    feat: remove book fun

--- src/index.js
+++ src/index.js
@@ -14,15 +14,20 @@ function Book(title, author, pagesNumber, isAlreadyRead) {
 
 Book.prototype.info = function info() { return `Title: ${this.title}, Author: ${this.author}, Pages Number: ${this.pagesNumber}, Is Already Read?: ${this.isAlreadyRead} `; };
 
-// * Tests requirement 3
+function showBooksInLibraryByName() {
+  return myLibrary.map((book) => book.title);
+}
+
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+}
+
+// * Tests requirement 5
 
 const favBook = new Book('Leer y Escribir', 'Alberto Masferrer1', '+50', true);
 const favBook2 = new Book('Minimum Vital', 'Alberto Masferrer2', '+11', true);
 const favBook3 = new Book('Dinero Maldito', 'Alberto Masferrer3', '+10', true);
 const favBook4 = new Book('Verdad', 'Alberto Masferrer4', '+1', true);
 
-function showBooksInLibraryByName() {
-  return myLibrary.map((book) => book.title);
-}
-
+console.log(removeBookFromLibrary());
 console.log(showBooksInLibraryByName());
