965de9d242f434cd2b4a5c09df26d6f67c669197
es-rene99 <es.rene99@gmail.com>
2021-05-03 19:02:46 -0600

    feat: adding book to library

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const myLibrary = [];
 
+function addBookToLibrary(bookObj) {
+  myLibrary.push(bookObj);
+}
+
 function Book(title, author, pagesNumber, isAlreadyRead) {
   this.title = title;
   this.author = author;
   this.pagesNumber = pagesNumber;
   this.isAlreadyRead = isAlreadyRead;
+  addBookToLibrary(this);
 }
 
 Book.prototype.info = function info() { return `Title: ${this.title}, Author: ${this.author}, Pages Number: ${this.pagesNumber}, Is Already Read?: ${this.isAlreadyRead} `; };
 
 const favBook = new Book('Leer y Escribir', 'Alberto Masferrer', '+50', true);
-console.log(favBook.info());
+
+// * Tests
+
+console.log(myLibrary);
+console.log(myLibrary[0].info());
