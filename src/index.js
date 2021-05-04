0e6c3760ee7ccaeea6988dd350fb6e0664d1014d
es-rene99 <es.rene99@gmail.com>
2021-05-04 16:21:05 -0600

    feat: toggleReadStatus prototype fun

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ function addBookToLibrary(bookObj) {
   myLibrary.push(bookObj);
 }
 
-function Book(title, author, pagesNumber, isAlreadyRead) {
+function Book(title, author, pagesNumber, readStatus) {
   this.title = title;
   this.author = author;
   this.pagesNumber = pagesNumber;
-  this.isAlreadyRead = isAlreadyRead;
+  this.readStatus = readStatus;
   addBookToLibrary(this);
 }
 
-Book.prototype.info = function info() { return `Title: ${this.title}, Author: ${this.author}, Pages Number: ${this.pagesNumber}, Is Already Read?: ${this.isAlreadyRead} `; };
+Book.prototype = {
+  info: function info() { return `Title: ${this.title}, Author: ${this.author}, Pages Number: ${this.pagesNumber}, Is Already Read?: ${this.readStatus} `; },
+  toggleReadStatus: function toggleReadStatus() { this.readStatus = !this.readStatus; },
+
+};
 
 function showBooksInLibraryByName() {
   return myLibrary.map((book) => book.title);
@@ -22,12 +26,13 @@ function removeBookFromLibrary(index) {
   myLibrary.splice(index, 1);
 }
 
-// * Tests requirement 5
+// * Tests requirement 6 change read status
 
 const favBook = new Book('Leer y Escribir', 'Alberto Masferrer1', '+50', true);
 const favBook2 = new Book('Minimum Vital', 'Alberto Masferrer2', '+11', true);
 const favBook3 = new Book('Dinero Maldito', 'Alberto Masferrer3', '+10', true);
 const favBook4 = new Book('Verdad', 'Alberto Masferrer4', '+1', true);
 
-console.log(removeBookFromLibrary());
-console.log(showBooksInLibraryByName());
+console.log(favBook.info());
+console.log(favBook.toggleReadStatus());
+console.log(favBook.info());
