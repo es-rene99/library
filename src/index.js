02f8779b924b190c46db39ff024aad0861bdfe2d
es-rene99 <es.rene99@gmail.com>
2021-05-03 18:50:35 -0600

    refactor: using prototype for info function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
+const myLibrary = [];
+
 function Book(title, author, pagesNumber, isAlreadyRead) {
   this.title = title;
   this.author = author;
   this.pagesNumber = pagesNumber;
   this.isAlreadyRead = isAlreadyRead;
-  this.info = () => `Title: ${title}, Author: ${author}, Pages Number: ${pagesNumber}, Is Already Read?: ${isAlreadyRead} `;
 }
+
+Book.prototype.info = function info() { return `Title: ${this.title}, Author: ${this.author}, Pages Number: ${this.pagesNumber}, Is Already Read?: ${this.isAlreadyRead} `; };
 
 const favBook = new Book('Leer y Escribir', 'Alberto Masferrer', '+50', true);
 console.log(favBook.info());
