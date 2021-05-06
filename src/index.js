736315f3720332badfe2e1d03ada8d8abcdb39c0
es-rene99 <es.rene99@gmail.com>
2021-05-05 20:52:24 -0600

    feat: basic title population on UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const LIB_STORAGE = 'libraryStorage';
 const STORAGE_TYPE = 'localStorage';
+const MAIN_LIBRARY_NAME = 'main__library';
+const mainLibraryTarget = document.querySelector(`.${MAIN_LIBRARY_NAME}`);
+let libraryStorage = [];
 
 function storageAvailable(type) {
   let storage;
@@ -25,41 +28,40 @@ function storageAvailable(type) {
   }
 }
 
-const library = {
-  [LIB_STORAGE]: [],
+const libraryHandler = {
   addBookToLibrary(bookObj, libraryParam) {
     libraryParam.push(bookObj);
-    this.libraryStorage = libraryParam;
+    libraryStorage = libraryParam;
     this.setLibraryStorage();
   },
   showBooksInLibraryByName() {
-    return this.libraryStorage.map((book) => book.title);
+    return libraryStorage.map((book) => book.title);
   },
   removeBookFromLibrary(index, libraryParam) {
     libraryParam.splice(index, 1);
-    this.libraryStorage = libraryParam;
+    libraryStorage = libraryParam;
     this.setLibraryStorage();
   },
-  isLibraryStorageAvailable: storageAvailable(STORAGE_TYPE),
   setLibraryStorage() {
-    if (this.isLibraryStorageAvailable) {
-      localStorage.setItem(LIB_STORAGE, JSON.stringify(this.libraryStorage));
+    if (storageAvailable(STORAGE_TYPE)) {
+      localStorage.setItem(LIB_STORAGE, JSON.stringify(libraryStorage));
     } else {
       console.log('Storage not available');
     }
   },
   initLibraryStorage() {
-    if (this.isLibraryStorageAvailable) {
+    if (storageAvailable(STORAGE_TYPE)) {
       if (!localStorage.getItem(LIB_STORAGE)) {
         this.setLibraryStorage();
       } else {
         console.log(localStorage.library);
-        this.libraryStorage = JSON.parse(localStorage.getItem(LIB_STORAGE));
+        libraryStorage = JSON.parse(localStorage.getItem(LIB_STORAGE));
       }
     } else {
       console.log('Storage not available');
     }
   },
+  isLibraryEmpty: () => libraryStorage.length === 0,
 };
 
 function Book(title, author, pagesNumber, readStatus) {
@@ -67,7 +69,7 @@ function Book(title, author, pagesNumber, readStatus) {
   this.author = author;
   this.pagesNumber = pagesNumber;
   this.readStatus = readStatus;
-  library.addBookToLibrary(this, library[LIB_STORAGE]);
+  libraryHandler.addBookToLibrary(this, libraryStorage);
 }
 
 Book.prototype = {
@@ -75,17 +77,45 @@ Book.prototype = {
   toggleReadStatus: function toggleReadStatus() { this.readStatus = !this.readStatus; },
 };
 
-library.initLibraryStorage();
+const uiHandler = {
+  populateContent() {
+    if (libraryHandler.isLibraryEmpty()) {
+      const libraryEmptyMsg = document.createElement('p');
+      libraryEmptyMsg.textContent = 'Library is empty';
+      mainLibraryTarget.appendChild(libraryEmptyMsg);
+    }
+    libraryStorage.forEach((book, index) => {
+      const bookArticle = document.createElement('article');
+      bookArticle.setAttribute('data-book-index', index);
+      const bookArticleH1 = document.createElement('h1');
+      bookArticleH1.innerText = book.title;
+      bookArticle.appendChild(bookArticleH1);
+      mainLibraryTarget.appendChild(bookArticle);
+    });
+  },
+};
 
-// * TEST local storage
+// TODO left here, working as expected but needs refactor
 
+// * TEST UI population of contents
 const favBook = new Book('Leer y Escribir', 'Alberto Masferrer1', '+50', true);
 const favBook2 = new Book('Minimum Vital', 'Alberto Masferrer2', '+11', true);
 const favBook3 = new Book('Dinero Maldito', 'Alberto Masferrer3', '+10', true);
 const favBook4 = new Book('Verdad', 'Alberto Masferrer4', '+1', true);
-console.log(library.showBooksInLibraryByName());
-console.log(localStorage[LIB_STORAGE]);
-const favBook5 = new Book('Verdad2', 'Alberto Masferrer4', '+1', true);
-console.log(localStorage[LIB_STORAGE]);
 localStorage.clear();
-console.log(localStorage[LIB_STORAGE]);
+
+const mainApp = {
+  init() {
+    libraryHandler.initLibraryStorage();
+    uiHandler.populateContent();
+  },
+};
+
+mainApp.init();
+
+// console.log(library.showBooksInLibraryByName());
+// console.log(localStorage[LIB_STORAGE]);
+// const favBook5 = new Book('Verdad2', 'Alberto Masferrer4', '+1', true);
+// console.log(localStorage[LIB_STORAGE]);
+// localStorage.clear();
+// console.log(localStorage[LIB_STORAGE]);
